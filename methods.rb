8705d2fdf5dbdf8ac2ef95cef4857b0dd68a65e6
Jackie Tung <jackietung3@gmail.com>
2022-04-04 11:26:04 -0400

    commit tree 69aaf80f20bcbd5ed47a5bafcf71f0c434faebc2

--- methods.rb
+++ methods.rb
@@ -1,1 +1,36 @@
 # Your code here!
+# function myFunction(param) {
+#   console.log("Running myFunction");
+#   return param + 1;
+# }
+def print_name
+    puts "Hello, programmer!"
+end
+
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name="Naureen")
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1,num2)
+    return num1 + num2
+end
+
+def halve(x)
+    if x.is_a?(Integer)
+        x / 2
+    else
+        nil
+    end
+end
